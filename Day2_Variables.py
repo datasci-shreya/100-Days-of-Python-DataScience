c8118d442ccb2102141b5183cd4f27acd1488b37
datasci-shreya <shreyuborate@gmail.com>
2026-01-22 18:44:50 +0530

    Add files via upload

---
+++ Day2_Variables.py
@@ -0,0 +1,13 @@
+# Day 2: Python Variables
+
+name = "Shreya"
+age = 23
+course = "Python & Data Science"
+
+print("Name:", name)
+print("Age:", age)
+print("Course:", course)
+
+# Updating a variable
+age = age + 1
+print("Updated Age:", age)
