0bfb100c5ae74e212eacfdf406c94c5d1748fa98
datasci-shreya <shreyuborate@gmail.com>
2026-01-23 23:06:11 +0530

    Add files via upload

---
+++ RevisionAll.py
@@ -0,0 +1,14 @@
+# set
+print("_"*15)
+arr = {10,20,30,40,10}
+print(arr)
+print(type(arr))
+
+# dict
+print("_"*15)
+book = {"Name":"Let us c","Price":350.50,"Author":"Y kanetkar"}
+print(book)
+print(type(book))
+print(book["Print"])
+
+
