655eb844e932c925d66cfc1e1597fec350e6bda8
datasci-shreya <shreyuborate@gmail.com>
2026-01-23 23:10:00 +0530

    Update Mapping&SetDataType.py

--- Mapping&SetDataType.py
+++ Mapping&SetDataType.py
@@ -1,14 +1,15 @@
 # set
 
-arr = {10,20,30,40,10}
+arr = {10,10,20,30,40,50,20}
 print(arr)
 print(type(arr))
 
 # dict
-book = {"Name":"Let us c","Price":350.50,"Author":"Y kanetkar"}
+book = {"Name":"Python","Price":1000.10,"Author":"Gudio Van Rossum"}
 print(book)
 print(type(book))
 print(book["Print"])
+
 
 
 
