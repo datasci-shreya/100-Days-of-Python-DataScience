65b8032b36cf8b87e1154b4a47b7d670a68ef637
datasci-shreya <shreyuborate@gmail.com>
2026-01-23 23:02:07 +0530

    Update and rename Variableargs.py to Variables.py

--- Variables.py
+++ Variables.py
@@ -1,11 +1,12 @@
-#variable argument type
+#variables
 def Addition(*No):
     print(No)
-    print(type(No)) #tuple #double are allowed 
+    print(type(No)) 
     print(len(No))  
 
 def main():
     Addition(11,21,51,101)
 
 if __name__ == "__main__":
+
     main()
